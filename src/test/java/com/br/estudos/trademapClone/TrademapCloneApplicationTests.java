ae5c66934ad8c515445be646b330f9d32f8e944b
Murilo Ramos <okamael.adc@gmail.com>
2022-04-12 22:25:33 -0300

    Inicio Estrutura projeto

---
+++ src/test/java/com/br/estudos/trademapClone/TrademapCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.estudos.trademapClone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrademapCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
